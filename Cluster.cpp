afe5b55318468a6bee5dc1df9102e68fa18192f5
SashaFlaska <alexander.flaska@ucdenver.edu>
2016-02-24 19:30:24 -0700

    Added files via upload

---
+++ Cluster.cpp
@@ -0,0 +1,9 @@
+//
+//  File.cpp
+//  PA2
+//
+//  Created by Alexander Flaska on 2/24/16.
+//  Copyright © 2016 Alexander Flaska. All rights reserved.
+//
+
+#include "Cluster.h"
---
+++ Point.cpp
@@ -0,0 +1,11 @@
+//
+//  File.cpp
+//  PA2
+//
+//  Created by Alexander Flaska on 2/24/16.
+//  Copyright © 2016 Alexander Flaska. All rights reserved.
+//
+
+#include "Point.h"
+
+int swag; 
