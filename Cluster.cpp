a7962093d0bfb65ea2ee7cbb376f0fad231fb4e5
SashaFlaska <alexander.flaska@ucdenver.edu>
2016-03-02 21:43:07 -0700

    Code finally working. 9:42  on the due date.

--- Cluster.cpp
+++ Cluster.cpp
@@ -1,12 +1,280 @@
 //
-//  File.cpp
+//  Cluster.cpp
 //  PA2
 //
 //  Created by Alexander Flaska on 2/24/16.
 //  Copyright © 2016 Alexander Flaska. All rights reserved.
 //
 
+#include <cstdlib>
+#include <assert.h>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
 #include "Cluster.h"
 
+namespace Clustering
+{
+    
+    LNode::LNode(const Point &p, LNodePtr n = nullptr) : point(p)// node
+    {
+        point = p;
+        next = n;
+    }
+    
+    Cluster::Cluster()// default constructor
+    {
+        __size = 0;
+        __points = nullptr;
+    }
+    
+    Cluster::Cluster(const Cluster &cluster) : Cluster()// copy constructor
+    {
+        for(int count = 0; count < cluster.__size; ++count)
+        {
+            add(cluster[count]);
+        }
+    }
+    int Cluster::getSize() const// getsize, pretty obvious
+    {
+        return __size;
+    }
 
-int gitTest; 
+    
+    Cluster & Cluster::operator=(const Cluster &cluster)// overloaded = operator
+    {
+        if(this == &cluster)// returns if current is equal to the cluster.
+            return *this;
+        
+        LNodePtr current = __points;// lnodeptr's
+        LNodePtr next;
+        
+        
+        while (current != nullptr)// as long as it isn't null
+        {
+            next = current->next;// assign
+            delete current;// delete
+            current = next;// assign
+            __size--;// subtract 1 from size
+        }
+        
+        __size = 0;
+        __points = nullptr;
+        
+        LNodePtr rightPointer = cluster.__points;
+        
+        
+        for (int pos = 0; pos < cluster.getSize() ; ++pos)
+        {
+            add(rightPointer->point);
+            rightPointer = rightPointer->next;
+        }
+        return *this;
+    }
+    Cluster::~Cluster()// destructor
+    {
+        LNodePtr current;
+        while (__points != nullptr) {
+            
+            current = __points;
+            __points = current->next;
+            
+            delete current;
+        }
+        
+        
+        
+        
+    }
+    
+      void Cluster::add(const Point &point)
+    {
+        
+        LNodePtr node = new LNode(point, nullptr);// makes a new node using constructor
+        
+        if (__points == nullptr)// if it's null
+        {
+            __points = node;// put a node in
+        }
+        else if (__points->point > point)// using overloaded > check pointers
+        {
+            node->next = __points;// asign appropriately
+            __points = node;
+        }
+        else
+        {
+            LNodePtr current = __points, next = __points->next;// add the pointer
+            while (true)
+            {
+                if (next == nullptr || next->point > point)
+                {
+                    current->next = node;
+                    node->next = next;
+                    break;// break if this happens, otherwise weird things take place
+                }
+                else
+                {
+                    current = next;
+                    next = next->next;
+                }
+            }
+        }
+        
+        __size++;
+    }
+    
+    const Point &Cluster::remove(const Point &point)
+    {
+        LNodePtr previous = nullptr;// assign previous to null
+        LNodePtr current = __points;// and the current to whatever __points is
+        if (current !=nullptr && current->point == point)// if current not null, and it's equal to the pt,
+        {
+            __points = __points->next;// assign to next, delete current, decrement size, and return
+            delete current;
+            __size--;
+            return point;
+        }
+        while (current != nullptr && point != current->point)// if both not equal, make previous = current, and make current point to next.
+        {
+            previous =current;
+            current = current->next;
+        }
+        previous->next = current->next;// make previous the current, and delete the real current. decrement size. return.
+        delete current;
+        __size--;
+        return point;
+        
+    }
+    
+    bool Cluster::contains(const Point &point)
+    {
+        
+        LNodePtr current = __points;// make current =to points
+        
+        while (current != nullptr)// as long as not equal, check if current pointer is equal to point, otherwise make current=nextptr.
+        {
+            if (current->point == point)
+                return true;
+            else
+                current = current->next;
+            
+        }
+        
+        return false;
+    }
+    
+    const Point &Cluster::operator[](unsigned int index) const
+    {
+        assert(__points != nullptr && index < __size);// make sure that it's not null and it's not overflowing.
+        LNodePtr cursor = __points;
+        
+        for (int pos = 0; pos < index; ++pos)
+        {
+            cursor = cursor->next;
+        }
+        return cursor->point;
+    }
+    std::ostream &operator<<(std::ostream &outputStream, const Cluster &cluster)// overloaded <<
+    {
+        LNodePtr outputPointer = cluster.__points; // makes outputPointer, while it exists.
+        while (outputPointer)
+        {
+            outputStream << outputPointer->point;//finds the pointer, gives it an endl char, and equates it to next ptr.
+            outputStream << std::endl;
+            outputPointer = outputPointer->next;
+        }
+        return outputStream;
+    }
+    
+     std::istream &operator>>(std::istream &inputStream, Clustering::Cluster &cluster)
+    {
+        std::string tempString;// temp variable for getline
+        while (getline(inputStream,tempString) && inputStream.good())// while everthing is good to go
+        {
+            int tempInt = std::count(tempString.begin(), tempString.end(), ',');// temp int holds the size using std::count
+            Point point(tempInt+1);// makes a new point at size+1
+            std::stringstream ss(tempString);// string stream for the pointer
+            ss >> point;
+            cluster.add(point);// adds it
+        }
+        return inputStream;
+        
+    }
+    
+    bool operator!=(const Cluster &leftCluster, const Cluster &rightCluster)
+    {
+        return(!(leftCluster == rightCluster));
+    }
+    
+    Cluster &Cluster::operator+=(const Point &point)// overloaded+= uses add function
+    {
+        add(point);
+        return *this;
+    }
+    
+    Cluster &Cluster::operator-=(const Point &point)// overloaded-= using remove function
+    {
+        remove(point);
+        return *this;
+    }
+    
+    Cluster &Cluster::operator+=(const Cluster &cluster)// overloaded += for clusters
+    {
+        for(int pos = 0; pos < cluster.__size; ++pos)// lops, if it doesn't exist, add
+        {
+            if(!contains(cluster[pos]))
+                add(cluster[pos]);
+        }
+        return *this;
+    }
+    
+    Cluster &Cluster::operator-=(const Cluster &cluster)// same as above, just removes.
+    {
+        for(int count = 0; count < cluster.__size; ++count)
+        {
+            if(contains(cluster[count]))
+                remove(cluster[count]);
+        }
+        return *this;
+    }
+    
+    // functions below are super simple. Just returns using ovelroaded operators. 
+    const Cluster operator+(const Cluster &cluster, const Point &point)
+    {
+        Cluster c(cluster);
+       return  c+=point;
+    }
+    
+    const Cluster operator-(const Cluster &cluster, const Point &point)
+    {
+        Cluster c(cluster);
+        return c-=point;
+    }
+    
+    const Cluster operator+(const Cluster &cluster, const Cluster &cluster1)
+    {
+        Cluster c(cluster);
+        return  c+=cluster1;
+    }
+    
+    const Cluster operator-(const Cluster &cluster, const Cluster &cluster1)
+    {
+        Cluster c(cluster);
+       return  c-=cluster1;
+    }
+    bool operator==(const Cluster &leftCluster, const Cluster &rightCluster)
+    {
+        if (leftCluster.getSize() != rightCluster.getSize())// if the cluster sizes arent equal, return false
+            return false;
+        
+        for (int pos = 0; pos < leftCluster.getSize(); ++pos)// otherwise, loop, and check each one to see if they're equal.
+        {
+            if (leftCluster[pos] != rightCluster[pos])
+                return false;
+        }
+        
+        return true;// return true only if nothing is equal
+    }
+    
+
+}
--- Point.cpp
+++ Point.cpp
@@ -1,5 +1,5 @@
 //
-//  File.cpp
+//  Point.cpp
 //  PA2
 //
 //  Created by Alexander Flaska on 2/24/16.
@@ -8,4 +8,262 @@
 
 #include "Point.h"
 
-int swag; 
+#include <cmath>
+#include <algorithm> //min, max
+#include <string>
+#include <sstream>
+#include <iostream>
+
+#include "Point.h"
+
+using namespace std;
+using namespace Clustering;
+
+namespace Clustering
+{
+    
+    unsigned int Point::__idGen = 0;
+    
+    Point::Point(int dimensions)// default constructor; creates id, increments, and makes a new array.
+    {
+        __id = ++__idGen;
+        
+        
+        __dim = dimensions;
+        __values = new double[__dim];
+        for (int pos = 0; pos < __dim; ++pos)
+        {
+            __values[pos] = 0.0;
+        }
+    }
+    
+    Point::Point(int i, double *array)// construtor that already has an array passed, so it just assigns and inrements.
+    {
+        __dim = i;
+        __values = array;
+        __id = __idGen++;
+    }
+    
+    Point::Point(const Point &point) // copy constructor. Does everything the default does. 
+    {
+        __dim = point.__dim;
+        __values = new double[__dim];
+        __id = point.__id;
+        
+        for (int pos = 0; pos < __dim; ++pos)
+            __values[pos] = point.__values[pos];
+        
+        
+        
+    }
+    
+    Point &Point::operator=(const Point &point)// overloaded equals operator
+    {
+        if (&point == this)// returns if it's the thing it's equal to.
+        {
+            return *this;
+        }
+        else
+        {
+            for (int pos = 0; pos < __dim; ++pos)// loops and assigns all pointers in array to the array
+            {
+                this->__values[pos] = point.getValue(pos);
+            }
+            
+            return *this;
+        }
+    }
+    
+    double Point::distanceTo(const Point &point) const// distance to function. pythagorean theorem.
+    {
+        double totalDistance = 0.0;
+        for (int pos = 0; pos < __dim; ++pos)
+        {
+            totalDistance += pow((__values[pos] - point.__values[pos]), 2);
+        }
+        
+        totalDistance = sqrt(totalDistance);
+        
+        return totalDistance;
+    }
+
+    Point &Point::operator*=(double d)// overloaded *= operator.
+    {
+        for (int pos = 0; pos < __dim; ++pos)// loops, and *='s everything, and then returns pointer.
+            __values[pos] *= d;
+        return *this;
+    }
+    
+    Point &Point::operator/=(double d)// overloaded /= operators.
+    {
+        for (int pos = 0; pos < __dim; ++pos)// same as above, except / instead of *
+            __values[pos] /= d;
+        return *this;
+    }
+    
+    const Point Point::operator*(double d) const // overloaded * operator
+    {
+        Point newPoint(*this);// makes a new point, multiplies it, and returns, using overloaded *=
+        newPoint *= d;
+        
+        return newPoint;
+    }
+    
+    const Point Point::operator/(double d) const // overloaded / operator
+    {
+        Point newPoint(*this); // new point, divides, and returns using overloaded /=
+        newPoint /= d;
+        
+        return newPoint;
+    }
+  
+    double &Point::operator[](int index)// just returns the array at indices.
+    {
+        return __values[index];
+    }
+    
+    Point &operator+=(Point &leftPoint, const Point &rightPoint)// overloaded +=
+    {
+        for (int pos = 0; pos < leftPoint.__dim; ++pos)// loops, and +='s left to right, returns left.
+            leftPoint.__values[pos] += rightPoint.__values[pos];
+        
+        return leftPoint;
+    }
+    
+    Point &operator-=(Point &leftPoint, const Point &rightPoint)// overloaded-=
+    {
+        for (int pos = 0; pos < leftPoint.__dim; ++pos)// loops, and -='s left to right, returns left.
+            leftPoint.__values[pos] -= rightPoint.__values[pos];
+        
+        return leftPoint;
+    }
+    
+    const Point operator+(const Point &leftPoint, const Point &rightPoint)// overloaded +
+    {
+        Point newPoint(leftPoint);// new point, returns newpoint+= rightpoint using overloaded +=
+        
+        return newPoint+=rightPoint;
+    }
+    
+    const Point operator-(const Point &leftPoint, const Point &rightPoint)// overloaded -
+    {
+        Point newPoint(leftPoint);// new point using leftpoing, returns it -=right point using overloaded-=
+        
+        return newPoint -= rightPoint;
+    }
+    
+    bool operator==(const Point &leftPoint, const Point &rightPoint)// overloaded ==
+    {
+        if (leftPoint.__id != rightPoint.__id)// if not the same, false.
+            return false;
+        
+        for (int i = 0; i < std::max(leftPoint.__dim, rightPoint.__dim); ++i)// loop through to make the same check.
+        {
+            if (leftPoint.getValue(i) != rightPoint.getValue(i))
+                return false;
+        }
+        
+        // ID's are the same, values are the same
+        return true;// if not false, must be true.
+    }
+    
+    bool operator!=(const Point &leftPoint, const Point &rightPoint)
+    {
+        return(!(leftPoint == rightPoint));// returns if not = to each other.
+    }
+    
+    bool operator<(const Point &leftPoint, const Point &rightPoint)// overloaded <
+    {
+        
+        if (leftPoint != rightPoint)// if they're not equal
+        {
+            if (leftPoint.__dim == rightPoint.__dim)// check if they're equal
+            {
+                for (int i = 0; i < leftPoint.__dim; ++i)// loop and return the appropriate bool based on what's greater
+                {
+                    if (leftPoint.__values[i] > rightPoint.__values[i])
+                    return false;
+                    
+                    else if (leftPoint.__values[i] < rightPoint.__values[i])
+                    return true;
+                    
+                }
+            }
+        }
+        return false;
+    }
+    
+    bool operator>(const Point &leftPoint, const Point &rightPoint)// overloaded >
+    {
+        return operator<(rightPoint, leftPoint);// using overloaded < just return
+    }
+    
+    bool operator<=(const Point &leftPoint, const Point &rightPoint)// overloaded <= using overloaded operators
+    {
+        return !(leftPoint > rightPoint);
+    }
+    
+    bool operator>=(const Point &leftPoint, const Point &rightPoint)// overloaded >= using overloaded operators
+    {
+        return !(leftPoint<rightPoint);
+    }
+    
+    std::ostream &operator<<(ostream &outputStream, const Point &point)// overloaded << operator
+    {
+        int pos;
+        for (pos = 0; pos < point.__dim-1; ++pos)// loops
+            outputStream << point.__values[pos] << ", ";// opens ostream with the values in __values
+            outputStream << point.__values[pos];// makes sure there are commas
+        
+        return outputStream;
+    }
+    
+    std::istream &operator>>(std::istream &inputStream, Point &point)// overloaded >> operator, code taken from lecture, tweaked for function
+    {
+        
+       
+        string tempstring;
+        string buffer;
+        double d;
+        int index = -1;
+        
+        while (getline(inputStream, tempstring))
+        {
+            stringstream lineStream(tempstring);
+            
+            while (getline(lineStream, buffer, ','))
+            {
+                d = stod(buffer);
+                point.setValue(++index, d);
+            }
+            
+        }
+        return inputStream;
+    }
+    
+}
+// destructor, and getters/ setters. Pretty obvious as to what they do.
+Point::~Point()
+{
+    delete  [] __values;
+}
+
+int Point::getId() const
+{
+    return __id;
+}
+
+int Point::getDims() const
+{
+    return __dim;
+}
+
+void Point::setValue(int i, double d)
+{
+    __values[i] = d;
+}
+
+double Point::getValue(int i) const
+{
+    return __values[i];
+}
